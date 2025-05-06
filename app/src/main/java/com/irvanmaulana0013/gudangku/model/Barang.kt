1a6700d41719cb846407e9f19ff4d870d73f43b5
irvan maulana <irvanm166@gmail.com>
2025-05-06 15:34:27 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/irvanmaulana0013/gudangku/model/Barang.kt
@@ -0,0 +1,10 @@
+package com.irvanmaulana0013.gudangku.model
+
+data class Barang(
+    val id: Long,
+    val nama: String,
+    val kategori: String,
+    val jumlah: String,
+    val deskripsi: String,
+    val tanggal: String
+)
