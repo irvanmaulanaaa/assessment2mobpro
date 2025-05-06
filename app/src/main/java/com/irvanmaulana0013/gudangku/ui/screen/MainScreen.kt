c0eaafe1ad59da9635b11accad4803df1eb8ca28
irvan maulana <irvanm166@gmail.com>
2025-05-06 16:03:11 +0700

    Membuat tampilan list

--- app/src/main/java/com/irvanmaulana0013/gudangku/ui/screen/MainScreen.kt
+++ app/src/main/java/com/irvanmaulana0013/gudangku/ui/screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.irvanmaulana0013.gudangku.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.irvanmaulana0013.gudangku.R
+import com.irvanmaulana0013.gudangku.model.Barang
 import com.irvanmaulana0013.gudangku.ui.theme.GudangkuTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,10 +49,47 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "halo ges",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(barang = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(barang: Barang) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = barang.nama,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = barang.kategori,
+            maxLines = 1
+        )
+        Text(
+            text = barang.jumlah
+        )
+        Text(
+            text = barang.deskripsi,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = barang.tanggal)
+    }
 }
 
 @Preview(showBackground = true)
