6b9650d5909deb3777e8c18cd4a4ef32b69658c8
irvan maulana <irvanm166@gmail.com>
2025-05-06 15:07:14 +0700

    Menambahkan Navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
+    implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
